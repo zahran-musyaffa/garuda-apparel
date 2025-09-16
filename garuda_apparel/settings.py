59f5294c292057b8d649439dd2b1058e9d13439f
zahran-musyaffa <zahran.musyaffa@ui.ac.id>
2025-09-16 20:24:33 +0700

    debbuging pws error 2

--- garuda_apparel/settings.py
+++ garuda_apparel/settings.py
@@ -11,10 +11,12 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 import os
-from dotenv import load_dotenv
 from pathlib import Path
 
-load_dotenv()
+from dotenv import load_dotenv
+BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / ".env.prod")  # ✅ paksa baca .env.prod
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -152,3 +154,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+print("DB_PORT from env:", repr(os.getenv('DB_PORT')))

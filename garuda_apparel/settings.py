43fff6ab2ce8c71de8d03120f48348b46dc7f8e4
zahran-musyaffa <zahran.musyaffa@ui.ac.id>
2025-09-16 21:41:35 +0700

    debuggin 3

--- garuda_apparel/settings.py
+++ garuda_apparel/settings.py
@@ -14,8 +14,7 @@ import os
 from pathlib import Path
 
 from dotenv import load_dotenv
-BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(BASE_DIR / ".env.prod")  
+load_dotenv()  
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.

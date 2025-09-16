30767ecd7c8b36951c64270b2432160ba8b06836
zahran-musyaffa <zahran.musyaffa@ui.ac.id>
2025-09-16 21:24:42 +0700

    changin production value 1

--- garuda_apparel/settings.py
+++ garuda_apparel/settings.py
@@ -101,7 +101,7 @@ if PRODUCTION:
             'USER': os.getenv('DB_USER'),
             'PASSWORD': os.getenv('DB_PASSWORD'),
             'HOST': os.getenv('DB_HOST'),
-            'PORT': os.getenv('DB_PORT'),
+            'PORT': int(os.getenv('DB_PORT')),
             'OPTIONS': {
                 'options': f"-c search_path={os.getenv('SCHEMA', 'public')}"
             }
